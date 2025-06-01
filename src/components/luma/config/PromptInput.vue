6ead01bc7dc00974e4220c543820b0bcc82f5283
Daeun <050610220853@st.buh.edu.vn>
2025-06-01 16:33:47 +0700

    Update PromptInput.vue

--- src/components/luma/config/PromptInput.vue
+++ src/components/luma/config/PromptInput.vue
@@ -10,7 +10,7 @@
       type="textarea"
       class="prompt"
       :placeholder="$t('luma.placeholder.prompt')"
-      :maxlength="2000"
+      :maxlength="290404"
       show-word-limit
     />
   </div>
